d9e1af14253ceb8ce0528717089d7bb5a00cb947
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-12-03 18:39:51 +0100

    Made navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,34 @@
-import React from "react";
-import logo from "../images/logo.png";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.module.css";
 import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 import styles from "./Navbar.module.css";
-
 const Navbar = () => {
+  const navRef = useRef(null);
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle(styles.responsiveNav);
+  };
+
   return (
     <div className={styles.navbar}>
-      <div className={styles.logo}>
-        <img src={logo} alt="Sushi" />
-      </div>
-      <div className={styles.links}>
+      <img src={logo} alt="" />
+      <div className={styles.links} ref={navRef}>
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
-        <Link to="/about">About Us</Link>
+        <Link to="/about">About</Link>
         <Link to="/contacts">Contacts</Link>
+        <button
+          className={`${styles.navBtn} ${styles.navCloseBtn}`}
+          onClick={showNavbar}
+        >
+          <FaTimes />
+        </button>
       </div>
+      <button className={styles.navBtn} onClick={showNavbar}>
+        <FaBars />
+      </button>
     </div>
   );
 };
