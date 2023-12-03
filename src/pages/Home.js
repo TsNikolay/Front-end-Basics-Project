6a8ef070ec94e37ed1103af3464e53e39fed316a
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-12-03 19:25:55 +0100

    Home page content was added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import HomePage from "../components/pagesComponents/HomePage";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 };
 
 export default Home;
